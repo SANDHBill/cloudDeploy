cb5b7b73fb39087c7c9395e6f4e65ff580861981
hamed silatani <hamed@silatani.com>
2015-10-07 09:12:06 +0100

    trying to grt nginx working

--- puppet/manifests/hello_nginx.pp
+++ puppet/manifests/hello_nginx.pp
@@ -1,1 +1,12 @@
 class{ 'nginx':}
+
+nginx::resource::upstream { 'tomcat':
+    members => [
+        'localhost:8080',
+    ],
+}
+
+nginx::resource::vhost { 'myapp.example.com':
+    proxy => 'http://tomcat',
+}
+
